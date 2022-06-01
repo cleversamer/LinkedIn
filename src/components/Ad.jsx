cf6ec633ec84b0dc88d198e404e0291be5766f30
Samer A <thedev.samer@gmail.com>
2022-06-01 13:27:05 +0300

    finished building the ad section

---
+++ src/components/Ad.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Ad = () => {
+  return (
+    <Container>
+      <h5>
+        <Link to="/">Hiring in a hurry? - </Link>
+      </h5>
+
+      <p>
+        Find talented pros in record time with Upwork and keep business moving.
+      </p>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  min-height: 50px;
+  margin-top: 10px;
+  padding: 16px 0;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+  p {
+    font-size: 14px;
+    color: #434649;
+    font-weight: 600;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+export default Ad;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,41 @@
 import styled from "styled-components";
 import Header from "../components/Header";
+import Ad from "../components/Ad";
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <Ad />
+      <Layout>
+        {/* <Leftside /> */}
+
+        {/* <Main /> */}
+
+        {/* <Rightside /> */}
+      </Layout>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /* grid-template-row: auto; */
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
 
 export default Home;
