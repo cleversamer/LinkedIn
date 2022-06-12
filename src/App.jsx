07aee8f992a5c01c5da6ab92184a795aaffdd4c0
Samer A <thedev.samer@gmail.com>
2022-06-12 08:08:15 +0300

    enhanced the use of redux state managment & the performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
+import { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getUserAuth } from "./store/user";
-import styled from "styled-components";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
-const App = () => {
+const App = ({ store }) => {
   const user = useSelector(getUserAuth());
-  console.log(user);
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {});
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
-    <Container>
-      <Routes>
-        <Route path="/not-found" element={<NotFound />} />
+    <Routes>
+      <Route path="/not-found" element={<NotFound />} />
 
-        {user ? (
-          <Route path="/" element={<Home />} />
-        ) : (
-          <Route path="/" element={<Login />} />
-        )}
+      {user ? (
+        <Route path="/" element={<Home />} />
+      ) : (
+        <Route path="/" element={<Login />} />
+      )}
 
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Container>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 };
-
-const Container = styled.div``;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
+import App from "./App";
 import "./index.css";
 
 const store = configureStore();
@@ -11,7 +11,7 @@ const store = configureStore();
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <App store={store} />
     </Provider>
   </BrowserRouter>,
   document.getElementById("app")
